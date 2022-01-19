b0def4c391f5ebd8b4f97f0347e39450fbaa168b
Guilherme Batista <97068622+guissa@users.noreply.github.com>
2022-01-18 23:01:51 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,38 @@
+<script>
+        function insert(number) {
+
+            let numero = document.getElementById('resultado').innerHTML;
+            
+            document.getElementById('resultado').innerHTML = numero + number;
+        }
+
+        function clean() {
+            document.getElementById('resultado').innerHTML = "";
+        }
+
+        function back() {
+
+            let resultado = document.getElementById('resultado').innerHTML;
+            document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+        }
+
+        function calcular() {
+
+            let resultado = document.getElementById('resultado').innerHTML;
+            if(resultado) {
+
+                document.getElementById('resultado').innerHTML = eval(resultado);
+            }
+
+            else {
+
+                document.getElementById('resultado').innerHTML = "Calcule..."
+            }
+
+
+        }
+
+        
+
+
+    </script>
